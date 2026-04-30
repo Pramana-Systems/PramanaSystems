81334ce7a951dab6d34f9d92f70abe3b191f9f26
Pavan Dev Singh Charak <charak1987@gmail.com>
2026-04-30 07:00:51 +0530

    add governed trust-root rotation lineage

---
+++ scripts/sign-trust-rotation.ts
@@ -0,0 +1,39 @@
+import fs from "fs";
+
+import crypto from "crypto";
+
+const rotation =
+  fs.readFileSync(
+    "trust/trust-rotation.json",
+    "utf8"
+  );
+
+const privateKey =
+  fs.readFileSync(
+    "trust/manthan-root.key",
+    "utf8"
+  );
+
+const signature =
+  crypto.sign(
+    null,
+
+    Buffer.from(
+      rotation,
+      "utf8"
+    ),
+
+    privateKey
+  );
+
+fs.writeFileSync(
+  "trust/trust-rotation.sig",
+
+  signature.toString(
+    "base64"
+  )
+);
+
+console.log(
+  "Trust rotation signed"
+);
